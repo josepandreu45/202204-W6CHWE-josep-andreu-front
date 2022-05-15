6ffd04492512bf10a47026c81ac9fbc5d327a10f
josep andreu <josepa45@gmail.com>
2022-05-16 00:12:36 +0200

    Added icon header and changed color

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,16 +2,29 @@ import styled from "styled-components";
 
 const HeaderContainer = styled.div`
   margin: 15px;
-  padding: 5px;
-  color: yellow;
+  padding-bottom: 40px;
+  color: #d9a406;
   font-size: 25px;
   display: flex;
+  flex-direction: column;
   justify-content: center;
+  align-items: center;
+  h1 {
+    margin: 0;
+    margin-left: -10px;
+    margin-top: -30px;
+    width: 225px;
+  }
+  img {
+    width: 225px;
+    height: 225px;
+  }
 `;
 
 const Header = () => {
   return (
     <HeaderContainer>
+      <img src="https://img.icons8.com/color/344/star-wars.png" alt="logo" />
       <h1>ROBOTS</h1>
     </HeaderContainer>
   );
