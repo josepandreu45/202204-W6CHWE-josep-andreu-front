42dd92c484d9d95546b209d66ad66fd658d5d0e6
josep andreu <josepa45@gmail.com>
2022-05-15 18:45:11 +0200

    Tested RobotsList component

---
+++ src/mocks/mockRobots.js
@@ -0,0 +1,40 @@
+const mockRobots = [
+  {
+    _id: "627fa1264a1be5dc492c720d",
+    image:
+      " https://cdn0.iconfinder.com/data/icons/famous-character-vol-1-colored/48/JD-34-256.png",
+    name: "C3PO",
+    create: " 2022-05-21T22:00:00.000+00:00",
+    resistance: 8,
+    velocity: 1,
+  },
+  {
+    _id: "627fa3874a1be5dc492c720e",
+    image:
+      "https://cdn4.iconfinder.com/data/icons/heroes-villains-vol-1-colored/100/R2D2-256.png",
+    name: "R2D2",
+    create: " 2022-05-21T22:00:00.000+00:00",
+    resistance: 10,
+    velocity: 7,
+  },
+  {
+    _id: "6281029d6a452cf0be0ee55d",
+    image:
+      "https://https://icons-for-free.com/download-icon-r2d2+robot+starwars+icon-1320166698566079188_256.ico.iconfinder.com/data/icons/heroes-villains-vol-1-colored/100/R2D2-256.png",
+    name: "BB-8",
+    create: " 2022-05-21T22:00:00.000+00:00",
+    resistance: 7,
+    velocity: 9,
+  },
+  {
+    _id: "62810d6b6a452cf0be0ee55e",
+    image:
+      "https://https://cdn4.iconfinder.com/data/icons/smashicons-movies-flat/54/15_-_Grievous_Flat-512.png://icons-for-free.com/download-icon-r2d2+robot+starwars+icon-1320166698566079188_256.ico.iconfinder.com/data/icons/heroes-villains-vol-1-colored/100/R2D2-256.png",
+    name: "General Grievous",
+    create: " 2022-05-21T22:00:00.000+00:00",
+    resistance: 9,
+    velocity: 9,
+  },
+];
+
+export default mockRobots;
